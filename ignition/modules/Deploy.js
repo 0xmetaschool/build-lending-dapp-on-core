8418ceda7a4f078d690f223f80e51c8987a766e1
fareeha25 <130544719+fareeha25@users.noreply.github.com>
2024-07-04 11:53:16 +0500

    Update Deploy.js

--- ignition/modules/Deploy.js
+++ ignition/modules/Deploy.js
@@ -1,8 +1,1 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-module.exports = buildModule("CoreLoanPlatform", (m) => {
-
-  const CoreLoanPlatform = m.contract("CoreLoanPlatform", ["0x3786495f5d8a83b7bacd78e2a0c61ca20722cce3","0x6B0e7A1C756564bB0A0d12008BB6b964C836Cbc3"]);
-
-  return { CoreLoanPlatform };
-});
